4a9440f1b01a190ca66c1d922c91c6d81b07ba4e
Nikoletta <nikolettanod@gmail.com>
2026-08-28 22:38:57 -0400

    Finalizing project

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ from src.metrics import calculate_avg_metrics, save_avg_results
 
 def main() -> None:
     test_seed = 27082026
-    validation_seed = [37082027,
-                       47082027,
-                       57082027,
-                       67082027,
-                       77082027]
 
     final_eval_split = SplitConfig(split_id="final_eval_split_v1",
                                    split_seed=(test_seed, 0),
@@ -24,8 +19,10 @@ def main() -> None:
     cnn19_config = CNNConfig(batch_size=16, candidate_id=19, filters=(2, 4, 8), kernel_size=(29, 29, 29),
                              strides=(1, 1, 1), padding=('same', 'same', 'valid'), fc_units=8, threshold=0.4)
 
+    final_evaluation("logistic_regression", final_eval_split=final_eval_split)
+    final_evaluation("mlp", final_eval_split=final_eval_split)
     final_evaluation("cnn", final_eval_split=final_eval_split, config=cnn17_config)
-
+    final_evaluation("xgboost", final_eval_split=final_eval_split)
 
 
 if __name__ == "__main__":
