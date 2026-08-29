bdc1dff05b78047319a91caf8cd7f4938d8abf57
Nikoletta <nikolettanod@gmail.com>
2026-08-28 23:09:22 -0400

    Finalizing project

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from src.config import CNNConfig, SplitConfig
-
-from src.model_selection import final_evaluation, Model
-from src.data_pipeline import create_experiment_split, load_experiment_data, create_eval_split
-from src.load_data import download_dataset, load_dataset
-from src.metrics import calculate_avg_metrics, save_avg_results
+from src.model_selection import final_evaluation
+from src.data_pipeline import create_eval_split
 
 
 def main() -> None:
@@ -13,15 +10,18 @@ def main() -> None:
                                    split_seed=(test_seed, 0),
                                    test_fraction=0.10,
                                    validation_fraction=0.0)
+    create_eval_split(final_eval_split)
 
     cnn17_config = CNNConfig(batch_size=16, candidate_id=17, filters=(2, 4, 8), kernel_size=(29, 29, 29),
                              strides=(1, 1, 1), padding=('same', 'same', 'valid'), fc_units=8)
+
     cnn19_config = CNNConfig(batch_size=16, candidate_id=19, filters=(2, 4, 8), kernel_size=(29, 29, 29),
                              strides=(1, 1, 1), padding=('same', 'same', 'valid'), fc_units=8, threshold=0.4)
 
     final_evaluation("logistic_regression", final_eval_split=final_eval_split)
     final_evaluation("mlp", final_eval_split=final_eval_split)
     final_evaluation("cnn", final_eval_split=final_eval_split, config=cnn17_config)
+    final_evaluation("cnn", final_eval_split=final_eval_split, config=cnn19_config)
     final_evaluation("xgboost", final_eval_split=final_eval_split)
 
 
